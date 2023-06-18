3627e8893eb3d9e03799f38d068b2f9fb5b4c650
saipranayteja <pranayteja.ch@algonox.com>
2023-06-18 19:00:51 +0530

    sdfgv

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
-locals={
+locals {
     dns_word=var.env=="prod" ? "www":"${var.env}"
     dns_name=var.component=="frontend" ? "${local.dns_word}.${var.domain_name}":"${var.env}-${var.component}.${var.domain_name}"
 
