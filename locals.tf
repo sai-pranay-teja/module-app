edd1d085fce1f29212bfeec74b33eef34dab6694
saipranayteja <pranayteja.ch@algonox.com>
2023-07-02 14:12:57 +0530

    zsxdcf

--- locals.tf
+++ locals.tf
@@ -2,5 +2,5 @@ locals {
     dns_word=var.env=="prod" ? "www":"${var.env}"
     dns_name=var.component=="frontend" ? "${local.dns_word}.${var.domain_name}":"${var.env}-${var.component}.${var.domain_name}"
 
-    parameters=concat([var.component], var.parameters)
+    parameters=concat([var.component, "nexus"], var.parameters)
 }
