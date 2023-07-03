bee05c91bda5a7aaca588183abdcad1b52153a7d
saipranayteja <pranayteja.ch@algonox.com>
2023-07-03 16:07:44 +0530

    sxdcfvgb

--- userdata.sh
+++ userdata.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
+# #!/bin/bash
 
-sudo labauto ansible
+# sudo labauto ansible
+# sudo ansible-pull -i localhost, -U https://github.com/sai-pranay-teja/roboshop-ansible.git roboshop-app.yml -e env=dev -e components=${component} >/opt/ansible.log
